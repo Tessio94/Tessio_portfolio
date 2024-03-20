670f0adb64d9531341c8693b095ecac55144aee3
Tessio94 <simeklapan@gmail.com>
2024-03-20 13:26:38 +0100

    finish touches

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ import "./style.css";
 import image1 from "./images/moja_slika.jpg";
 import image2 from "./images/timer.jpg";
 import image3 from "./images/cartoon-developer.jpg";
+import image4 from "./images/prehab.png";
+import image5 from "./images/slider.png";
+import image6 from "./images/rps.png";
+import image7 from "./images/bankist.png";
+import image8 from "./images/popcorn.png";
 import files from "./files/ažurirani CV.pdf";
 import component from "./component";
 
