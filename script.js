efb3e5f65c83af697e94d81fa9e9d99130d813a7
Tessio94 <simeklapan@gmail.com>
2024-05-04 20:22:46 +0200

    changing project

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import image2 from "./images/timer.jpg";
 import image3 from "./images/cartoon-developer.jpg";
 import image4 from "./images/prehab.png";
 import image5 from "./images/slider.png";
-import image6 from "./images/rps.png";
+import image6 from "./images/zadarboattours.png";
 import image7 from "./images/bankist.png";
 import image8 from "./images/popcorn.png";
 import files from "./files/ažurirani CV.pdf";
